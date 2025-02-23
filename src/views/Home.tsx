0cd79d401bb9ad67b8a72f27c2da51f7426ace1d
Gabriel Caldubehere <caldubeheregabriel@gmail.com>
2025-02-23 20:26:28 -0300

    redes

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import perfil from '../assets/perfil circulo.png'
-import '../App.css'
-
-
+import React from "react";
+import perfil from "../assets/perfil circulo.png";
+import { BsGithub } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFileDownload } from "react-icons/fa";
+import "../App.css";
 
 function Home() {
   return (
-    <div className='Home'>
-        <div className='titulo'>
-        <h1>Gabriel Caldubehere</h1>
-        <h2>Desarrollador Front End</h2>   
+    <div className="Home">
+      <div className="titulo">
+        <h1 className="nombre">Gabriel Caldubehere</h1>
+        <h2 className="subtitulo">Desarrollador Front End</h2>
+        <div className="redes-sociales">
+          <a href="https://github.com/gabrielcaldubehere" target="_blank">
+            <BsGithub size={50} color="green" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gabrielcaldubehere"
+            target="_blank"
+          >
+            <FaLinkedin size={50} color="orange" />
+          </a>
+          <a href="/CV Hector Gabriel Caldubehere.pdf" download>
+            <FaFileDownload size={50} color="#7e13b0" />
+          </a>
         </div>
+      </div>
 
-
-        <img className='perfil' src={perfil} alt="perfil" />
+      <img className="perfil" src={perfil} alt="perfil" />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
